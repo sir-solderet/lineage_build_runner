45d95eb250c9364ba0bef5f9effe2f3d548fba75
Solderet <31514860+sir-solderet@users.noreply.github.com>
2025-08-07 18:39:33 +0530

    Update LUNCH_COMBO

--- scripts/build.sh
+++ scripts/build.sh
@@ -2,7 +2,7 @@
 set -e
 
 source build/envsetup.sh
-lunch ${LUNCH_COMBO:-lineage_gta9-bp3a-userdebug}
+lunch ${LUNCH_COMBO:-lineage_gta9-bp1a-userdebug}
 
 export TARGET_BUILD_RECOVERY_IMAGE=true
 make bacon -j$(nproc --all)
